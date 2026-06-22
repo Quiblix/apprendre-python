8ea9990d5bc272d25ba1ee1ff2ac64c55ffcd09b
martin_voron <martin_voron@carrefour.com>
2026-06-22 11:10:28 +0000

    ajout de la gestion d'erreur

--- calculatrice/main.py
+++ calculatrice/main.py
@@ -3,8 +3,13 @@ operation = input("Entrez l'opération (+, -, *, /) : ")
 nombre2 = float(input("Entrez le deuxième nombre : "))
 
 def main():
-    resultat = eval(f"{nombre1} {operation} {nombre2}")
-    print(resultat)
+    if nombre2 == 0:
+        nombre3 = float(input("Division par zéro impossible. Entrez le troisième nombre :"))
+        resultat = eval(f"{nombre1} {operation} {nombre3}")
+        print("resultat :", resultat)
+    else:
+        resultat = eval(f"{nombre1} {operation} {nombre2}")
+        print("resultat :", resultat)
 
 if __name__ == "__main__":
     main()
